9b3ba887be323ddf93fda7037dcab28addab394c
York <joriksp@hotmail.com>
2018-04-22 14:31:24 +0200

    Roling window, correct values?

--- Mental Health-A1/main_NT.py
+++ Mental Health-A1/main_NT.py
@@ -28,7 +28,7 @@ dAgg = dataset.groupby(['id', 'variable', dataset['time'].dt.date])['value'].mea
 
 #Create dummy frame to fill with values of moving average
 dataset2 = pd.DataFrame(loadData('Data/dataset_mood_smartphone.csv'))
-new_df = dataset.groupby(['id', 'variable', dataset['time'].dt.date])['value'].mean()
+new_df = dataset2.groupby(['id', 'variable', dataset['time'].dt.date])['value'].mean()
 
 idx = pd.IndexSlice
 window_size = 5
@@ -52,12 +52,16 @@ for usr in dAgg.axes[0].levels[0]: # loop over all users to generate time window
     print('usr == == ==', usr)
     
     for d in range(len(dAgg.axes[0].levels[2]) - window_size):
+        if d == 0:
+            print('window: ', dAgg.axes[0].levels[2][d], dAgg.axes[0].levels[2][d+window_size-1])
+            print('slice: ', dAgg.loc[idx[usr,:,slice(dAgg.axes[0].levels[2][d],dAgg.axes[0].levels[2][d+window_size-1])]].mean(level='variable'))
+        
         insert = dAgg.loc[idx[usr,:,slice(dAgg.axes[0].levels[2][d],dAgg.axes[0].levels[2][d+window_size-1])]].mean(level='variable')
         #print(new_df[usr])
         #print(new_df.loc[usr])
         new_df.loc[idx[usr,:,dAgg.axes[0].levels[2][d]]].value = insert
                    #,:,dAgg.axes[0].levels[1],dAgg.axes[0].levels[2][d]]
-
+moving = dAgg.rolling(window_size).mean()
 
 #MultiIndex(levels=[['AS14.01', 'AS14.02', 'AS14.03', 'AS14.05', 'AS14.06', 'AS14.07', 'AS14.08', 'AS14.09', 'AS14.12', 'AS14.13', 'AS14.14', 'AS14.15', 'AS14.16', 'AS14.17', 'AS14.19', 'AS14.20', 'AS14.23', 'AS14.24', 'AS14.25', 'AS14.26', 'AS14.27', 'AS14.28', 'AS14.29', 'AS14.30', 'AS14.31', 'AS14.32', 'AS14.33'], 
 #['activity', 'appCat.builtin', 'appCat.communication', 'appCat.entertainment', 'appCat.finance', 'appCat.game', 'appCat.office', 'appCat.other', 'appCat.social', 'appCat.travel', 'appCat.unknown', 'appCat.utilities', 'appCat.weather', 'call', 'circumplex.arousal', 'circumplex.valence', 'mood', 'screen', 'sms'],
